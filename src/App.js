07b6d73d858665c784495e57b297f4456afbdc8e
Rathodpreeti <103851456+Rathodpreeti@users.noreply.github.com>
2022-05-06 20:56:51 +0530

    Task 2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,47 @@
+import Users from "./Components/Card";
 import "./styles.css";
 
-export default function App() {
-  return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { users_data: [], loading: true };
+
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState() {
+    const link = "https://reqres.in/api/users?page=1";
+    fetch(link)
+      .then((response) => response.json())
+      .then((users) => {
+        this.setState({ users_data: users.data, loading: false });
+        console.log(users.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  render() {
+    return (
+      <>
+        <nav>
+          <div className="navbar_container">
+            <div className="navbar_items">
+              <h2>LetsGrowMore</h2>
+              <button onClick={this.updateState}>Get Users</button>
+            </div>
+          </div>
+        </nav>
+        <div className="box">
+          <Users loading={this.state.loading} users={this.state.users_data} />
+        </div>
+      </>
+    );
+  }
 }
+
+export default App;
